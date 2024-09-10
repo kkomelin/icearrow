f529e4c4505a5ee60f4438ab47a34829dbac3257
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-09-10 12:53:10 +0200

    Switched to Google Fonts for the Roboto font to speed up the deployment.

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -2,10 +2,12 @@ import { createRoot } from 'react-dom/client';
 import { Suspense } from 'react';
 import App from './App';
 import './i18n';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+
+// @todo: Load fonts from filesystem. Currently using Google Fonts to simplify the deployment. See index.html
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
