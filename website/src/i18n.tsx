fd1735e7b8bfe096985718df5bc5767d3118000f
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-09-11 12:40:48 +0200

    Slightly improved commands. Got rid of the CJS Vite warning. Removed debug output.

--- website/src/i18n.tsx
+++ website/src/i18n.tsx
@@ -3,8 +3,6 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
-console.log((import.meta.env.VITE_BASE_URL || '') + '/locales/{{lng}}.json', 'ddd');
-
 i18n
   .use(initReactI18next)
   .use(Backend)
