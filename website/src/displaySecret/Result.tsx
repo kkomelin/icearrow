d7901b9bd0f69e5c3f30f80162a9136b425ec2ce
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-09-12 19:19:08 +0200

    Improved the link page. Added Share button.

--- website/src/displaySecret/Result.tsx
+++ website/src/displaySecret/Result.tsx
@@ -1,71 +1,55 @@
 import { faCopy } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  Box,
-  Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableRow,
-} from '@mui/material';
+import { Box, Button, useTheme } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useCopyToClipboard } from 'react-use';
+import ShareButton from '../createSecret/ShareButton';
 
 type ResultProps = {
   readonly uuid: string;
   readonly password: string;
   readonly prefix: 's' | 'f';
-  readonly customPassword?: boolean;
 };
 
-const Result = ({ uuid, password, prefix, customPassword }: ResultProps) => {
+const Result = ({ uuid, password, prefix }: ResultProps) => {
   const base =
     (import.meta.env.VITE_BASE_URL ||
       `${window.location.protocol}//${window.location.host}`) + `/#/${prefix}`;
   const short = `${base}/${uuid}`;
   const full = `${short}/${password}`;
   const { t } = useTranslation();
+  const [copy, copyToClipboard] = useCopyToClipboard();
+  const theme = useTheme();
 
   return (
     <Box>
-      {/* <Typography variant="h4">{t('result.title')}</Typography>
-      <Typography>
-        {t('result.subtitleDownloadOnce')}
-        <br />
-        {t('result.subtitleChannel')}
-      </Typography> */}
-      <TableContainer>
-        <Table>
-          <TableBody>
-            <Row label={t('result.rowLabelOneClick')} value={full} />
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Box
+        sx={{
+          border: '2px solid' + theme.palette.primary.main,
+          p: 2,
+          borderRadius: '6px',
+          mb: 2,
+        }}
+      >
+        {full}
+      </Box>
+
+      <Box sx={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
+        <Box>
+          <Button
+            startIcon={<FontAwesomeIcon icon={faCopy} />}
+            color={copy.error ? 'secondary' : 'primary'}
+            variant="contained"
+            onClick={() => copyToClipboard(full)}
+          >
+            Copy
+          </Button>
+        </Box>
+        <Box>
+          <ShareButton url={full} />
+        </Box>
+      </Box>
     </Box>
-  );
-};
-
-type RowProps = {
-  readonly label: string;
-  readonly value: string;
-};
-
-const Row = ({ label, value }: RowProps) => {
-  const [copy, copyToClipboard] = useCopyToClipboard();
-  return (
-    <TableRow key={label}>
-      <TableCell width="15">
-        <Button
-          color={copy.error ? 'secondary' : 'primary'}
-          variant="contained"
-          onClick={() => copyToClipboard(value)}
-        >
-          <FontAwesomeIcon icon={faCopy} />
-        </Button>
-      </TableCell>
-      <TableCell>{value}</TableCell>
-    </TableRow>
   );
 };
 
